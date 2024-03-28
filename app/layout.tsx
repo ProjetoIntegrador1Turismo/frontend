fff8cb2e8ae98be62caae74f13ab0dbb4ce6d321
bernardotonin <btprates2004@gmail.com>
2024-03-28 20:46:58 -0300

    feat: add navbar component to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,11 @@ import type { Metadata } from 'next';
 import './globals.css';
 import React from 'react';
 import { poppins } from '../lib/fonts';
+import Navbar from '@/components/Navbar/Navbar';
 
 export const metadata: Metadata = {
   title: 'TourLink',
-  description: 'TourLink is the worlds top leading tour finding platform.'
+  description: 'TourLink is the worlds top leading Tourist Guide finding platform.'
 };
 
 export default function RootLayout({
@@ -15,7 +16,10 @@ export default function RootLayout({
 }>) {
   return (
     <html className={poppins.className} lang='en'>
-      <body>{children}</body>
+      <body>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
