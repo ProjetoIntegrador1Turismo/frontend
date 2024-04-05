ca60f800737b3e175e46db02747bf7dde6525816
bernardotonin <btprates2004@gmail.com>
2024-04-05 16:13:36 -0300

    feat: create register schemas

--- schemas/index.ts
+++ schemas/index.ts
@@ -4,5 +4,22 @@ export const LoginSchema = z.object({
   email: z.string().email({
     message: 'Insira um e-mail válido!'
   }),
-  password: z.string().min(1, { message: 'Senha inválida' })
+  password: z.string().min(5, { message: 'Senha deve ter no minimo 5 caracteres!' })
+});
+
+export const RegisterSchema = z.object({
+  email: z.string().email({
+    message: 'Insira um e-mail válido!'
+  }),
+  password: z.string().min(5, { message: 'Senha deve ter no minimo 5 caracteres!' }),
+  name: z.string().min(1, { message: 'Nome é obrigatório!' })
+});
+
+export const RegisterGuideSchema = z.object({
+  email: z.string().email({
+    message: 'Insira um e-mail válido!'
+  }),
+  password: z.string().min(5, { message: 'Senha deve ter no minimo 5 caracteres!' }),
+  name: z.string().min(1, { message: 'Nome é obrigatório!' }),
+  cadastur: z.string().min(1, { message: 'Cadastur é obrigatório para o cadastro de guia!' })
 });
