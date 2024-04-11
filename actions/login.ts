e7ff5b6e82b444bceb8087ec96f45f3cc67573d6
bernardotonin <btprates2004@gmail.com>
2024-04-11 15:11:59 -0300

    feat: implement login action

--- actions/login.ts
+++ actions/login.ts
@@ -1,13 +1,34 @@
 'use server';
 import { LoginSchema } from '@/schemas';
 import * as z from 'zod';
+import { signIn } from '@/auth';
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
+import { AuthError } from 'next-auth';
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedFields = LoginSchema.safeParse(values);
 
   if (!validatedFields.success) {
-    return { error: 'Usuário ou senha incorreto!' };
+    return { error: 'Campos invalidos!' };
   }
 
-  return { success: 'Sucesso! Redirecionando você agora!' };
+  const { email, password } = validatedFields.data;
+  try {
+    await signIn('credentials', {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT
+    });
+    return { success: 'Sucesso! redirecionando você agora!' };
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return { error: 'Usuário ou senha incorreto!' };
+        default:
+          return { error: 'Erro ao fazer login.' };
+      }
+    }
+    throw error;
+  }
 };
