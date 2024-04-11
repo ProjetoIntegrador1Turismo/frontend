4b8508b0e3105a4e647350a81b66c6e810d0d5b4
bernardotonin <btprates2004@gmail.com>
2024-04-11 15:09:45 -0300

    chore: configure next auth credential provider

--- auth.ts
+++ auth.ts
@@ -1,9 +1,39 @@
 import NextAuth from 'next-auth';
-import GitHub from 'next-auth/providers/github';
+import Credentials from 'next-auth/providers/credentials';
+import { LoginSchema } from './schemas';
 
 export const {
   handlers: { GET, POST },
-  auth
+  auth,
+  signIn,
+  signOut
 } = NextAuth({
-  providers: [GitHub]
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const validatedFields = LoginSchema.safeParse(credentials);
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+          const authResponse = await fetch('https://dummyjson.com/auth/login', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              username: email,
+              password: password,
+              expiresInMins: 3
+            })
+          });
+
+          if (!authResponse.ok) {
+            return null;
+          }
+          const user = await authResponse.json();
+          return user;
+        }
+        return null;
+      }
+    })
+  ]
 });
