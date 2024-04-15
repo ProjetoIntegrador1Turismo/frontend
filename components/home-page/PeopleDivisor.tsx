818d102e49e9b7985d8660df25511ba0d0bd0231
bernardotonin <btprates2004@gmail.com>
2024-04-15 19:13:08 -0300

    feat: create divisor components

---
+++ components/home-page/PeopleDivisor.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Users } from 'lucide-react';
+const PeopleDivisor = () => {
+  return (
+    <div className='flex items-center my-3'>
+      <div className='border-t border-black w-1/2 mr-2'></div>
+      <Users />
+      <div className='border-t border-black w-1/2 ml-2'></div>
+    </div>
+  );
+};
+
+export default PeopleDivisor;
