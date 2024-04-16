2297501cc92fb67f692f25dde50712c6600d7e66
bernardotonin <btprates2004@gmail.com>
2024-04-16 15:43:43 -0300

    style: add navbar divisor

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -3,14 +3,18 @@ import styles from '../../styles/Navbar/Navbar.module.css';
 import NavbarButtonsContainer from './NavBarButtonsContainer';
 import { NavBarButtons } from '@/lib/config';
 import AuthArea from './AuthArea';
+import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <div className={styles.NavbarContainer}>
-      <div>Logo</div>
-      <NavbarButtonsContainer buttons={NavBarButtons} />
-      <AuthArea />
-    </div>
+    <>
+      <div className={styles.NavbarContainer}>
+        <Link href='/'>Logo</Link>
+        <NavbarButtonsContainer buttons={NavBarButtons} />
+        <AuthArea />
+      </div>
+      <div className={styles.Divisor}></div>
+    </>
   );
 };
 
