dc2871307dc4538a4b4e7ad30b962164ed180e47
bernardotonin <btprates2004@gmail.com>
2024-04-22 17:43:18 -0300

    chore: wrap application with session provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,23 +3,28 @@ import './globals.css';
 import React from 'react';
 import { poppins } from '../lib/fonts';
 import Navbar from '@/components/Navbar/Navbar';
+import SessionProvider from '@/components/Auth/SessionProvider';
+import { auth } from '@/auth';
 
 export const metadata: Metadata = {
   title: 'TourLink',
   description: 'TourLink is the worlds top leading Tourist Guide finding platform.'
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth();
   return (
     <html className={poppins.className} lang='en'>
-      <body>
-        <Navbar />
-        {children}
-      </body>
+      <SessionProvider session={session}>
+        <body>
+          <Navbar />
+          {children}
+        </body>
+      </SessionProvider>
     </html>
   );
 }
