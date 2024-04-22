e256311d594a0bb5a063a3f9f46a5868f2e2d32e
bernardotonin <btprates2004@gmail.com>
2024-04-22 17:56:03 -0300

    style: add more font weights

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,3 +1,3 @@
 import { Poppins } from 'next/font/google';
 
-export const poppins = Poppins({ weight: ['200', '400', '600'], subsets: ['latin'] });
+export const poppins = Poppins({ weight: ['200', '300', '400', '600'], subsets: ['latin'] });
