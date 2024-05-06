6cd7904a448a13dfbee5f92d4045df0d77b62668
bernardotonin <btprates2004@gmail.com>
2024-05-06 13:30:57 -0300

    feat: add week guides component

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,13 @@
 import PlaneDivisor from '@/components/home-page/PlaneDivisor';
 import MainCard from '@/components/home-page/MainCard';
-import { mockSlides, mockTour } from '@/lib/mocks';
+import { mockGuides, mockSlides, mockTour } from '@/lib/mocks';
 import SliderWithHeader from '@/components/home-page/SliderWithHeader';
 import BondeDivisor from '@/components/home-page/BondeDivisor';
+import WeekGuides from '@/components/home-page/WeekGuides';
 
 export default function Home() {
   return (
     <div>
-      {/* <form
-        action={async () => {
-          'use server';
-          await signOut({ redirectTo: '/login' });
-        }}
-      >
-        <button type='submit'>Sign Out</button>
-      </form> */}
       <div className='flex flex-col gap-4 xl:flex-row justify-around'>
         <MainCard tour={mockTour} />
         <MainCard tour={mockTour} />
@@ -24,6 +17,8 @@ export default function Home() {
       <SliderWithHeader title='Restaurantes, Eventos e Passeios' slides={mockSlides} />
       <SliderWithHeader title='Roteiros, Hóteis e Expêriencias' slides={mockSlides} />
       <BondeDivisor />
+      <WeekGuides guides={mockGuides} />
+      <div className='mb-14' />
     </div>
   );
 }
