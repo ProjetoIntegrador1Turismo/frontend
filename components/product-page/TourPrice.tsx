a1e8357ae032ff7113ea6c13eeadcc2aa2691f8d
bernardotonin <btprates2004@gmail.com>
2024-06-29 19:14:08 -0300

    feat: create tour price component

---
+++ components/product-page/TourPrice.tsx
@@ -0,0 +1,25 @@
+import { TourPriceProps } from '@/lib/interfaces';
+import React from 'react';
+
+const TourPrice = ({ price }: TourPriceProps) => {
+  switch (price) {
+    case 1:
+      return (
+        <p className='tracking-[10px] text-xl select-none'>
+          $ <span className='text-gray-400'>$ $</span>
+        </p>
+      );
+    case 2:
+      return (
+        <p className='tracking-[10px] text-xl select-none'>
+          $ $ <span className='text-gray-400'>$</span>
+        </p>
+      );
+    case 3:
+      return <p className='tracking-[10px] text-xl select-none'>$ $ $</p>;
+    default:
+      break;
+  }
+};
+
+export default TourPrice;
