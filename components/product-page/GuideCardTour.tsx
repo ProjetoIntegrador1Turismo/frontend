640a542a420797f820cecc849f0cc6c75f022d31
bernardotonin <btprates2004@gmail.com>
2024-06-29 19:12:55 -0300

    feat: create guide card from tour page

---
+++ components/product-page/GuideCardTour.tsx
@@ -0,0 +1,37 @@
+import { GuideCardProps } from '@/lib/interfaces';
+import Rating from '../home-page/Rating';
+import Image from 'next/image';
+import InterestButton from './InterestButton';
+
+const GuideCardTour = ({ guide }: GuideCardProps) => {
+  return (
+    <div className='grid grid-cols-[70px_1fr_1fr_1fr_270px] gap-4 items-center border-black border p-2 rounded-full min-w-[950px] max-w-[950px] max-h-[85px] shadow-lg shadow-gray-400'>
+      <Image
+        className='w-[70px] h-[70px] object-cover rounded-full'
+        src={guide.img}
+        alt='Guide Image'
+        width={70}
+        height={70}
+      />
+      <div className='flex flex-col'>
+        <p className='font-light text-sm'>Nome</p>
+        <h1 className='font-semibold text-lg truncate' style={{ maxWidth: '180px' }}>
+          {guide.name}
+        </h1>
+      </div>
+      <div className='flex flex-col'>
+        <p className='font-light text-sm mb-[5px]'>Avaliação</p>
+        <Rating rating={guide.rating} />
+      </div>
+      <div className='flex flex-col'>
+        <p className='font-light text-sm'>Viagens realizadas</p>
+        <h1 className='font-semibold text-2xl'>{guide.trips}</h1>
+      </div>
+      <div className='flex justify-end items-center'>
+        <InterestButton />
+      </div>
+    </div>
+  );
+};
+
+export default GuideCardTour;
