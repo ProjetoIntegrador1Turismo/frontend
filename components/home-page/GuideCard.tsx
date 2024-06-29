20ce22e23d72d09e9928e070bf334de17ebdf22e
bernardotonin <btprates2004@gmail.com>
2024-06-29 19:11:57 -0300

    refactor: change guidecard css logic

--- components/home-page/GuideCard.tsx
+++ components/home-page/GuideCard.tsx
@@ -1,28 +1,33 @@
 import { DotsHorizontalIcon } from '@radix-ui/react-icons';
 import { GuideCardProps } from '@/lib/interfaces';
 import Rating from './Rating';
+import Image from 'next/image';
 
 const GuideCard = ({ guide }: GuideCardProps) => {
   return (
-    <div className='flex justify-between border-black border-[1px] p-2 items-center rounded-full min-w-[750px] max-w-[750px] max-h-[85px]'>
-      <img
+    <div className='grid grid-cols-[70px_1fr_1fr_1fr_30px] gap-4 items-center border-black border p-2 rounded-full min-w-[750px] max-w-[750px] max-h-[85px] shadow-lg shadow-gray-400'>
+      <Image
         className='w-[70px] h-[70px] object-cover rounded-full'
         src={guide.img}
         alt='Guide Image'
+        width={70}
+        height={70}
       />
-      <div>
+      <div className='flex flex-col'>
         <p className='font-light text-sm'>Nome</p>
-        <h1 className='font-semibold text-2xl'>{guide.name}</h1>
+        <h1 className='font-semibold text-lg truncate' style={{ maxWidth: '180px' }}>
+          {guide.name}
+        </h1>
       </div>
-      <div>
-        <p className='font-light text-sm'>Avaliação</p>
+      <div className='flex flex-col'>
+        <p className='font-light text-sm mb-[5px]'>Avaliação</p>
         <Rating rating={guide.rating} />
       </div>
-      <div>
+      <div className='flex flex-col'>
         <p className='font-light text-sm'>Viagens realizadas</p>
         <h1 className='font-semibold text-2xl'>{guide.trips}</h1>
       </div>
-      <div>
+      <div className='flex justify-end items-center'>
         <a>
           <DotsHorizontalIcon width={30} height={30} />
         </a>
