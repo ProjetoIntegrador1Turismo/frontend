e23ee32b845963e6adfa5f7d9facd9d5954c0a49
bernardotonin <btprates2004@gmail.com>
2024-06-29 19:13:24 -0300

    feat: create product interest button component

---
+++ components/product-page/ProductButton.tsx
@@ -0,0 +1,16 @@
+import { PlaneIcon } from 'lucide-react';
+import React from 'react';
+
+const ProductButton = () => {
+  return (
+    <a
+      href='#guides'
+      className='bg-gradient-to-t w-[450px] shadow-lg shadow-gray-400 justify-center from-tl-red text-3xl h-fit items-center to-tl-purple rounded-full flex p-6 gap-2 text-white font-extrabold hover:from-tl-red-2 hover:to-tl-purple-2'
+    >
+      <h1>Tenho interesse!</h1>
+      <PlaneIcon height={36} width={36} />
+    </a>
+  );
+};
+
+export default ProductButton;
