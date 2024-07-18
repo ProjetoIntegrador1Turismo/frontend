6c379878bd8f30ddb732249be381915984d1c176
bernardotonin <btprates2004@gmail.com>
2024-07-18 15:43:30 -0300

    feat: implement sketch of register.ts

--- actions/register.ts
+++ actions/register.ts
@@ -1,21 +1,31 @@
 'use server';
-import { RegisterUser } from '@/api/service';
-import { LoginSchema, RegisterSchema } from '@/schemas';
-import { useRouter } from 'next/router';
+import { RegisterSchema } from '@/schemas';
 import * as z from 'zod';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
-
   const validatedFields = RegisterSchema.safeParse(values);
 
   if (!validatedFields.success) {
     return { error: 'Não foi possivel concluir seu cadastro' };
   }
 
-  const status = await RegisterUser(validatedFields.data);
+  const { email, name, password } = validatedFields.data;
 
-  if (status === false) {
-    return { error: 'Email já está em uso.' };
+  const response = await fetch('', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      username: email,
+      password: password,
+      name: name
+    })
+  });
+
+  if (!response.ok) {
+    //error implementation
   }
+
   return { success: 'Sucesso! Redirecionando você agora!' };
 };
