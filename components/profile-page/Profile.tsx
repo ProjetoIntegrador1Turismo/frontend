931a3746539b804bb2c6d03eb88aa0dd3d895087
Guilherme <guilherme_laz@hotmail.com>
2024-07-24 21:55:35 -0300

    feat: implement session datas

--- components/profile-page/Profile.tsx
+++ components/profile-page/Profile.tsx
@@ -1,12 +1,15 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import ProfileInfo from './ProfileInfo';
+import { auth } from '@/auth';
 
 export async function Profile() {
+  const session = await auth();
+
   return (
-    <Tabs defaultValue='info' className='flex'>
+    <Tabs defaultValue='info' className='flex gap-4'>
       <TabsList className='flex flex-col h-fit gap-12 border border-black rounded-xl p-4'>
         <h1 className='font-bold text-3xl text-black break-words w-48 text-center'>
-          Kleber Rodrigues da Silva
+          {session?.user?.firstName}
         </h1>
         <img src='/avatar.jpg' className='w-[150px] h-[150px] rounded-full object-cover' />
         <TabsTrigger className='rounded-xl shadow-md shadow-gray-400' value='info'>
