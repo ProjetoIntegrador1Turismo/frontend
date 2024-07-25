e4307a79ad985a7f06dc418a922f7674c3023862
Guilherme <guilherme_laz@hotmail.com>
2024-07-24 21:51:28 -0300

    chore: implement session typing

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,9 +1,12 @@
-// import NextAuth from 'next-auth';
-// implement session user typing
-// declare module 'next-auth' {
-//   interface Session {
-//     user: {
-//         ...data
-//     };
-//   }
-// }
+import NextAuth from 'next-auth';
+declare module 'next-auth' {
+  interface Session {
+    user: {
+        id: string;
+        firstName: string;
+        lastName: string;
+        email: string;
+        image: string;
+    };
+  }
+}
