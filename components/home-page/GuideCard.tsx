d5c198edf37f6b56096746cafc4e04081853dbcd
Guilherme <guilherme_laz@hotmail.com>
2024-07-24 21:55:03 -0300

    refactor: implement data from api

--- components/home-page/GuideCard.tsx
+++ components/home-page/GuideCard.tsx
@@ -8,7 +8,7 @@ const GuideCard = ({ guide }: GuideCardProps) => {
     <div className='grid grid-cols-[70px_1fr_1fr_1fr_30px] gap-4 items-center border-black border p-2 rounded-full min-w-[750px] max-w-[750px] max-h-[85px] shadow-lg shadow-gray-400'>
       <Image
         className='w-[70px] h-[70px] object-cover rounded-full select-none pointer-events-none'
-        src={guide.img}
+        src={``}
         alt='Guide Image'
         width={70}
         height={70}
@@ -16,12 +16,12 @@ const GuideCard = ({ guide }: GuideCardProps) => {
       <div className='flex flex-col'>
         <p className='font-light text-sm select-none'>Nome</p>
         <h1 className='font-semibold text-lg truncate' style={{ maxWidth: '180px' }}>
-          {guide.name}
+          {guide.firstName}
         </h1>
       </div>
       <div className='flex flex-col'>
         <p className='font-light text-sm mb-[5px] select-none'>Avaliação</p>
-        <Rating rating={guide.rating} />
+        <Rating rating={guide.averageRating} />
       </div>
       <div className='flex flex-col'>
         <p className='font-light text-sm select-none'>Viagens realizadas</p>
