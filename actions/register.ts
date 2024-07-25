2f562ce21f423600d51b64ef32b2725230753029
Guilherme <guilherme_laz@hotmail.com>
2024-07-24 21:52:29 -0300

    feat: implement register server action

--- actions/register.ts
+++ actions/register.ts
@@ -1,13 +1,21 @@
 'use server';
-import { LoginSchema } from '@/schemas';
+import { RegisterUser } from '@/api/service';
+import { LoginSchema, RegisterSchema } from '@/schemas';
+import { useRouter } from 'next/router';
 import * as z from 'zod';
 
-export const register = async (values: z.infer<typeof LoginSchema>) => {
-  const validatedFields = LoginSchema.safeParse(values);
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
+
+  const validatedFields = RegisterSchema.safeParse(values);
 
   if (!validatedFields.success) {
     return { error: 'Não foi possivel concluir seu cadastro' };
   }
 
+  const status = await RegisterUser(validatedFields.data);
+
+  if (status === false) {
+    return { error: 'Email já está em uso.' };
+  }
   return { success: 'Sucesso! Redirecionando você agora!' };
 };
