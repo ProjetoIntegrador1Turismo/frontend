cc16866718fd0694836d4201dbbbb6d1e7c8df69
bernardotonin <btprates2004@gmail.com>
2024-07-27 12:47:22 -0300

    feat: create admin tabs component

---
+++ components/admin-panel/AdminTabs.tsx
@@ -0,0 +1,32 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+
+export async function AdminTabs() {
+  return (
+    <Tabs defaultValue='interestpoint' className='flex gap-4'>
+      <TabsList className='flex flex-col h-fit gap-12 border border-black rounded-xl p-4'>
+        <h1 className='font-bold text-3xl text-black break-words w-48 text-center'>
+          <p>admin</p>
+        </h1>
+        <img src='/avatar.jpg' className='w-[150px] h-[150px] rounded-full object-cover' />
+        <TabsTrigger className='rounded-xl shadow-md shadow-gray-400' value='interestpoint'>
+          Cadastrar Pontos
+        </TabsTrigger>
+        <TabsTrigger className='rounded-xl shadow-md shadow-gray-400' value='config'>
+          Config
+        </TabsTrigger>
+        <TabsTrigger className='rounded-xl shadow-md shadow-gray-400' value='guides'>
+          Histórico
+        </TabsTrigger>
+      </TabsList>
+      <TabsContent value='interestpoint'>
+        <p>cadastro de lugar</p>
+      </TabsContent>
+      <TabsContent value='config'>
+        <p>settings</p>
+      </TabsContent>
+      <TabsContent value='guides'>
+        <p>aprovar guias</p>
+      </TabsContent>
+    </Tabs>
+  );
+}
