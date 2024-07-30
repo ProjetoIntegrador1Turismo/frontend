fe85806daf76358dc551f54d7ce4516012e8ceb3
bernardotonin <btprates2004@gmail.com>
2024-07-30 15:38:36 -0300

    style: adjust layout for admin page

--- app/(protected)/admin/layout.tsx
+++ app/(protected)/admin/layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const layout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  return <div className='h-screen flex justify-center'>{children}</div>;
 };
 
 export default layout;
