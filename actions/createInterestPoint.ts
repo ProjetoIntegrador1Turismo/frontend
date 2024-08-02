e991cb7f09416e55afd861db70cba567be610361
Guilherme <guilherme_laz@hotmail.com>
2024-08-01 21:49:45 -0300

    feat: implement create interest point server action functionality

--- actions/createInterestPoint.ts
+++ actions/createInterestPoint.ts
@@ -2,18 +2,20 @@
 import * as z from 'zod';
 import { InterestPointFormSchema } from '@/schemas';
 import { interestPointCreate } from '@/api/service';
-import { error } from 'console';
 
 export const createInterestPoint = async (values: z.infer<typeof InterestPointFormSchema>) => {
   const validatedFields = InterestPointFormSchema.safeParse(values);
 
   if (!validatedFields.success) {
-    return { error: `${validatedFields.error}` };
+    return { error: 'Campos invalidos!' };
   }
 
   const status = await interestPointCreate(validatedFields.data);
 
-  
+  if (!status) {
+    return { error: 'Erro ao criar ponto de interesse!' };
+    
+  }
 
-  return { success: `${JSON.stringify(status)}` };
+  return { success: 'Seu ponto de interesse foi criado com sucesso!' };
 };
