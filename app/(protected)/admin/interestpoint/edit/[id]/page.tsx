fc82e76c40e11d52f88190ce7e69762841f4ec72
Guilherme <guilherme_laz@hotmail.com>
2024-08-05 21:58:47 -0300

    feat: create interest point edit page

---
+++ app/(protected)/admin/interestpoint/edit/[id]/page.tsx
@@ -0,0 +1,16 @@
+import { auth } from "@/auth";
+import InterestPointEditForm, { InterestPointData } from "@/components/admin-panel/InterestPointEditForm";
+
+const InterestPointEditPage = async ({ params }: { params: { id: string } }) => {
+  const session = await auth();
+  const response = await fetch(`http://localhost:8081/interestpoint/${params.id}`, {
+    headers: { Authorization: `Bearer ${session?.user.authToken}` }
+  });
+  const interestPointData = (await response.json()) as InterestPointData;
+
+  return <div>
+    <InterestPointEditForm InterestPoint={interestPointData} />
+  </div>;
+};
+
+export default InterestPointEditPage;
