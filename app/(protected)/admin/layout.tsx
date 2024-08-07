f9ae3ead76377ef807a5bbc1175e39719584eb8c
Guilherme <guilherme_laz@hotmail.com>
2024-08-06 21:59:13 -0300

    style: adjust placement

--- app/(protected)/admin/layout.tsx
+++ app/(protected)/admin/layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const layout = ({ children }: { children: React.ReactNode }) => {
-  return <div className='h-screen flex justify-center'>{children}</div>;
+  return <div className='flex justify-center'>{children}</div>;
 };
 
 export default layout;
--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const AdminPage = async () => {
   return (
-    <div>
+    <div className='h-[75vh]'>
       <AdminTabs />
     </div>
   );
