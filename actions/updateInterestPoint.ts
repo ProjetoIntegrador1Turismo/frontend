f40d6e2776f36de6cadad4ce636a6b19155ad26d
bernardotonin <btprates2004@gmail.com>
2024-08-08 21:54:59 -0300

    chore: tweak updateInterestPoint server action

--- actions/updateInterestPoint.ts
+++ actions/updateInterestPoint.ts
@@ -1,21 +1,20 @@
 'use server';
 import * as z from 'zod';
-import { InterestPointFormSchema } from '@/schemas';
-import { interestPointCreate, interestPointUpdate } from '@/api/service';
+import { InterestPointEditFormSchema } from '@/schemas';
+import { interestPointUpdate } from '@/api/service';
 
-export const updateInterestPoint = async (values: z.infer<typeof InterestPointFormSchema>, id: string) => {
-  const validatedFields = InterestPointFormSchema.safeParse(values);
+export const updateInterestPoint = async (values: z.infer<typeof InterestPointEditFormSchema>, id: number) => {
+  const validatedFields = InterestPointEditFormSchema.safeParse(values);
 
   if (!validatedFields.success) {
     return { error: 'Campos invalidos!' };
   }
-
+  
   const status = await interestPointUpdate(validatedFields.data, id);
 
   if (!status) {
-    return { error: 'Erro ao criar ponto de interesse!' };
-    
+    return { error: 'Erro ao editar ponto de interesse!' };
   }
 
-  return { success: 'Seu ponto de interesse foi criado com sucesso!' };
+  return { success: 'Seu ponto de interesse foi editado com sucesso!' };
 };
