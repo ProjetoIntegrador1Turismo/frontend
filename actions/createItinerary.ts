0ea1ed5941d3adf86e513893dc9a1c41a536c9a4
bernardotonin <btprates2004@gmail.com>
2024-08-15 17:02:54 -0300

    feat: create createItinerary server action

---
+++ actions/createItinerary.ts
@@ -0,0 +1,21 @@
+'use server';
+import * as z from 'zod';
+import { NewItineraryFormSchema } from '@/schemas';
+import { ItineraryCreate } from '@/api/service';
+
+export const createItinerary = async (values: z.infer<typeof NewItineraryFormSchema>) => {
+  const validatedFields = NewItineraryFormSchema.safeParse(values);
+
+  // if (!validatedFields.success) {
+  //   return { error: 'Campos invalidos!' };
+  // }
+
+  const status = await ItineraryCreate(values);
+
+  if (!status.ok) {
+    return { error: 'Erro ao criar roteiro!' };
+    
+  }
+
+  return { success: 'Seu roteiro foi criado com sucesso!', id: status.id };
+};
