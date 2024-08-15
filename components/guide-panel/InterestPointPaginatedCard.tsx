02a0ee8ba892058dad8b96dd09d8096db80d1d2a
Guilherme <guilherme_laz@hotmail.com>
2024-08-14 21:45:28 -0300

    feat: create interest point paginated card

---
+++ components/guide-panel/InterestPointPaginatedCard.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+const InterestPointPaginatedCard = ({
+  id,
+  imageCoverUrl,
+  name,
+  onClick
+}: {
+  id: number;
+  imageCoverUrl: string;
+  name: string;
+  onClick?: () => void;
+}) => {
+  return (
+    <button onClick={onClick}>
+      <div className='h-[150px] max-w-[250px]'>
+        <Image
+          className='w-[300px] h-[100px] rounded-t-xl object-cover'
+          src={imageCoverUrl}
+          alt={name}
+          width={300}
+          height={300}
+        />
+        <div className='bg-gradient-to-r from-tl-red to-tl-purple rounded-b-xl flex items-center justify-center'>
+          <h1 className='text-white text-xl font-bold p-3 truncate max-w-[300px]'>{name}</h1>
+        </div>
+      </div>
+    </button>
+  );
+};
+
+export default InterestPointPaginatedCard;
