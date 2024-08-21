bc7d573cc98e9adc52506fa6418166c95aca8f45
Guilherme <guilherme_laz@hotmail.com>
2024-08-21 09:43:54 -0300

    chore: update GuideTabs to show Itineraries component

--- components/guide-panel/GuideTabs.tsx
+++ components/guide-panel/GuideTabs.tsx
@@ -2,10 +2,11 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import Itineraries from './ItinerariesPaginated';
 import NewItineraryForm from './NewItineraryForm';
 import InterestedTourists from './InterestedTourists';
+import { getAuthToken } from '@/api/service';
 
 export async function GuideTabs() {
   return (
-    <Tabs defaultValue='interestpoint' className='flex gap-4'>
+    <Tabs defaultValue='itineraries' className='flex gap-4'>
       <TabsList className='flex flex-col h-fit gap-12 border border-black rounded-xl p-4'>
         <h1 className='font-bold text-3xl text-black break-words w-48 text-center'>
           <p>Guia</p>
@@ -23,6 +24,8 @@ export async function GuideTabs() {
         </TabsTrigger>
       </TabsList>
       <TabsContent value='itineraries'>
+        <div className='h-[85vh]'>
+        </div>
       </TabsContent>
       <TabsContent value='newItinerary'>
         <NewItineraryForm />
