646e1281ba8b849cb892b2a7fa1d3b4dfd340d8b
bernardotonin <btprates2004@gmail.com>
2024-09-04 14:08:02 -0600

    feat: implement api data fetching in guide profile tabs

--- components/guide-profile/GuideProfileTabs.tsx
+++ components/guide-profile/GuideProfileTabs.tsx
@@ -1,14 +1,49 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import axios from 'axios';
+import { redirect } from 'next/navigation';
+import GuideItineraries from './GuideItineraries';
+import GuideReviews from './GuideReviews';
 
-export async function GuideProfileTabs() {
+export interface GuideProfilePageSource {
+  id: number
+  firstName: string
+  lastName: string
+  cadasturCode: string
+  averageRating: any
+  profileImageUrl: string
+  reviews: Review[]
+  itineraries: Itinerary[]
+}
+
+export interface Itinerary {
+  id: number
+  title: string
+  imageCoverUrl: string
+}
+
+export interface Review {
+  touristName: string
+  avatarUrl: string
+  id: number
+  text: string
+  date: string
+  rating: number
+}
+
+export async function GuideProfileTabs({id}: { id: number }) {
+  let guideData: GuideProfilePageSource;
+  try {
+    const request = await axios.get<GuideProfilePageSource>(`http://localhost:8081/page-source/guide/${id}`);
+    guideData = request.data;
+  } catch (error) {
+    redirect('/');
+  }
 
   return (
     <Tabs defaultValue='itineraries' className='flex gap-4'>
       <TabsList className='flex flex-col h-fit gap-12 border border-black rounded-xl p-4'>
-        <h1 className='font-bold text-3xl text-black break-words w-48 text-center'>
-            Ola guia
-        </h1>
-        <img src='/avatar.jpg' className='w-[150px] h-[150px] rounded-full object-cover' />
+        <h1 className='font-bold text-3xl text-black break-words w-48 text-center'>{`${guideData.firstName} ${guideData.lastName}`}</h1>
+        <img src={guideData.profileImageUrl} className='w-[150px] h-[150px] rounded-full object-cover select-none' draggable={false} />
         <TabsTrigger className='rounded-xl shadow-md shadow-gray-400' value='itineraries'>
           Roteiros
         </TabsTrigger>
@@ -17,10 +52,10 @@ export async function GuideProfileTabs() {
         </TabsTrigger>
       </TabsList>
       <TabsContent value='itineraries'>
-        <p>Roteiros</p>
+        <GuideItineraries itineraries={guideData.itineraries} />
       </TabsContent>
       <TabsContent value='reviews'>
-        <p>Reviews</p>
+        <GuideReviews reviews={guideData.reviews} />
       </TabsContent>
     </Tabs>
   );
