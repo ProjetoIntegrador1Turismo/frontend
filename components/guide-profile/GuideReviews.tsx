d0d8e3d5d39802d31ce8347bba49d2365c2944eb
bernardotonin <btprates2004@gmail.com>
2024-09-09 18:49:57 -0300

    style: structure reviews in a grid format

--- components/guide-profile/GuideReviews.tsx
+++ components/guide-profile/GuideReviews.tsx
@@ -2,24 +2,34 @@ import React from 'react';
 import { Review } from './GuideProfileTabs';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../ui/card';
 import CommentCard from './CommentCard';
+import { CommentFormDialog } from './CommentFormDialog';
 
-const GuideReviews = ({ reviews }: { reviews: Review[] }) => {
+const GuideReviews = ({
+  reviews,
+  guideId,
+  guideName
+}: {
+  reviews: Review[];
+  guideId: number;
+  guideName: string;
+}) => {
   if (reviews.length === 0) {
     return <p>Wow, que vazio!</p>;
   }
 
   return (
-    <div className='min-h-[75vh] h-fit'>
+    <div className='min-h-[75vh] h-fit w-fit'>
       <Card>
         <CardHeader>
           <CardTitle>Reviews do guia</CardTitle>
           <CardDescription>
-            Aqui você encontra todos as avaliações que esse guia recebeu!
+            Aqui você encontra todos as avaliações que {guideName} recebeu!
           </CardDescription>
+          <CommentFormDialog guideId={guideId} guideName={guideName} />
         </CardHeader>
-        <CardContent>
+        <CardContent className='grid grid-cols-3 grid-rows-2 gap-4'>
           {reviews.map((review) => {
-            return <CommentCard review={review} />;
+            return <CommentCard review={review} key={review.id} />;
           })}
         </CardContent>
       </Card>
