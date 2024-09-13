4662e163b9c81b05a9df4f77ddea4d873b0be78c
bernardotonin <btprates2004@gmail.com>
2024-09-13 00:39:44 -0300

    refactor: tweak session typing

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,17 +1,19 @@
 import NextAuth from 'next-auth';
 declare module 'next-auth' {
   interface Session {
-    user: {
-      authToken: string;
-      firstName: string;
-      lastName: string;
-      email: string;
-      userType: string;
-      authTokenExpiresIn: string;
-      refreshToken: string;
-      refreshTokenExpiresIn: string;
-      cadasturCode?: string;
-      profileImageUrl: string;
-    };
+    user: User;
+  }
+  interface User {
+    authToken: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    userType: string;
+    authTokenExpiresIn: string;
+    refreshToken: string;
+    refreshTokenExpiresIn: string;
+    cadasturCode?: string;
+    profileImageUrl: string;
+    authTokenExpirationTime: number;
   }
 }
