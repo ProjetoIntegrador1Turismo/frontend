ff0aca48913dc91f9794c3ddd13172748336ff86
Guilherme <guilherme_laz@hotmail.com>
2024-09-15 02:36:38 -0300

    chore: changed pagination method and buttons

--- lib/interfaces.ts
+++ lib/interfaces.ts
@@ -3,6 +3,7 @@ import { ReactNode } from 'react';
 export interface NavBarButtonProps {
   buttonText: string;
   href: string;
+  highlight?: boolean;
 }
 
 export interface NavBarButtonsContainerProps {
