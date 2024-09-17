37cce29705dde10140c55cf060922dcb4ae877aa
bernardotonin <btprates2004@gmail.com>
2024-09-17 00:45:49 -0300

    feat: create guide itinerary card component

---
+++ components/itinerary-page/GuideCardItinerary.tsx
@@ -0,0 +1,51 @@
+import { PlaneIcon } from 'lucide-react';
+import Rating from '../home-page/Rating';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const GuideCardItinerary = ({
+  img,
+  name,
+  rating,
+  id
+}: {
+  img: string;
+  name: string;
+  rating: number;
+  id: number;
+}) => {
+  return (
+    <div className='grid grid-cols-[70px_1fr_1fr_270px] gap-4 items-center border-black border p-2 rounded-full min-w-[750px] max-w-[750px] max-h-[85px] shadow-lg shadow-gray-400'>
+      <Link href={`/guide-profile/${id}`}>
+        <Image
+          className='w-[70px] h-[70px] object-cover rounded-full select-none'
+          src={img}
+          alt='Guide Image'
+          width={60}
+          height={60}
+        />
+      </Link>
+
+      <div className='flex flex-col justify-center'>
+        <p className='font-light text-sm leading-none select-none'>Nome</p>
+        <h1
+          className='font-semibold text-lg truncate'
+          style={{ minWidth: '220px', maxWidth: '220px' }}
+        >
+          {name}
+        </h1>
+      </div>
+      <div className='flex flex-col justify-center'>
+        <p className='font-light text-sm leading-none mb-1 select-none'>Avaliação</p>
+        <Rating rating={rating} />
+      </div>
+      <div className='flex justify-end items-center'>
+        <button className='w-[250px] h-[70px] bg-gradient-to-t  from-tl-red items-center text-xl to-tl-purple rounded-full flex justify-center gap-2 text-white font-extrabold hover:from-tl-red-2 hover:to-tl-purple-2 select-none'>
+          Tenho interesse <PlaneIcon />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default GuideCardItinerary;
