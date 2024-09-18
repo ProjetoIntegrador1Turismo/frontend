2a6c1f63503f4c35e94d4db36b1cb24ec3311b77
bernardotonin <btprates2004@gmail.com>
2024-09-17 21:34:19 -0300

    feat: add select top 3 screen to admin tabs

--- components/admin-panel/AdminTabs.tsx
+++ components/admin-panel/AdminTabs.tsx
@@ -4,7 +4,8 @@ import GuideApproval from './GuideApproval';
 import InterestPoints from './InterestPoints';
 import { auth } from '@/auth';
 import Image from 'next/image';
-import { MapPinIcon, Pen, PersonStanding } from 'lucide-react';
+import { MapPinIcon, Medal, Pen, User } from 'lucide-react';
+import SelectTop3Form from './SelectTop3Form';
 
 export async function AdminTabs() {
   const session = await auth();
@@ -22,23 +23,41 @@ export async function AdminTabs() {
           alt={session?.user.firstName ?? ' '}
         />
         <TabsTrigger className='rounded-xl shadow-md shadow-gray-400' value='interestpoint'>
-          <MapPinIcon /> | Cadastrar Pontos
+          <div className='flex gap-1 items-center'>
+            <MapPinIcon height={15} width={15} />
+            Cadastrar Pontos
+          </div>
         </TabsTrigger>
-        <TabsTrigger className='rounded-xl shadow-md shadow-gray-400' value='config'>
-          <Pen /> | Editar Pontos
+        <TabsTrigger className='rounded-xl shadow-md shadow-gray-400' value='editpoints'>
+          <div className='flex gap-1 items-center'>
+            <Pen height={15} width={15} />
+            Editar Pontos
+          </div>
         </TabsTrigger>
         <TabsTrigger className='rounded-xl shadow-md shadow-gray-400' value='guides'>
-          <PersonStanding /> | Gerenciar Guias
+          <div className='flex gap-1 items-center'>
+            <User height={15} width={15} />
+            Gerenciar Guias
+          </div>
+        </TabsTrigger>
+        <TabsTrigger className='rounded-xl shadow-md shadow-gray-400' value='top3'>
+          <div className='flex gap-1 items-center'>
+            <Medal height={15} width={15} />
+            Selecionar Top 3
+          </div>
         </TabsTrigger>
       </TabsList>
       <TabsContent value='interestpoint'>
         <InterestPointForm />
       </TabsContent>
-      <TabsContent value='config'>
+      <TabsContent value='editpoints'>
         <InterestPoints />
       </TabsContent>
       <TabsContent value='guides'>
         <GuideApproval />
+      </TabsContent>
+      <TabsContent value='top3'>
+        <SelectTop3Form />
       </TabsContent>
     </Tabs>
   );
