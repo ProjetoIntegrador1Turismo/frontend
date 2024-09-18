f484da76f12d1e8989b8127767e866016c63d312
bernardotonin <btprates2004@gmail.com>
2024-09-18 18:37:54 -0300

    feat: check if its itinerary or tour to navigate to the correct path

--- components/home-page/HomeSlider.tsx
+++ components/home-page/HomeSlider.tsx
@@ -5,7 +5,7 @@ import {
   CarouselNext,
   CarouselPrevious
 } from '../ui/carousel';
-import { FirstSlider, HomeSliderProps } from '@/lib/interfaces';
+import { FirstSlider } from '@/lib/interfaces';
 import SliderCard from './SliderCard';
 
 const HomeSlider = ({ slides }: { slides: FirstSlider[] }) => {
@@ -20,7 +20,12 @@ const HomeSlider = ({ slides }: { slides: FirstSlider[] }) => {
       <CarouselContent>
         {slides.map((tour, index) => (
           <CarouselItem key={index} className='md:basis-1/3 lg:basis-1/6 sm:basis-1/2'>
-            <SliderCard image={tour.imageCoverUrl} title={tour.name} id={tour.id} />
+            <SliderCard
+              image={tour.imageCoverUrl}
+              title={tour.name}
+              id={tour.id}
+              type={tour.interestPointType === 'ITINERARY' ? 'itinerary' : 'tour'}
+            />
           </CarouselItem>
         ))}
       </CarouselContent>
