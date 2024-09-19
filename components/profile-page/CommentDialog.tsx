23ad1fabcefc97022d596095296d9e8f42b6fa49
bernardotonin <btprates2004@gmail.com>
2024-09-19 07:00:54 -0300

    feat: implement delete comment functionality

--- components/profile-page/CommentDialog.tsx
+++ components/profile-page/CommentDialog.tsx
@@ -1,14 +1,43 @@
-import {
-  Dialog,
-  DialogContent,
-  DialogFooter,
-  DialogTrigger,
-  DialogClose
-} from '@/components/ui/dialog';
+'use client';
+import { Dialog, DialogContent, DialogFooter, DialogTrigger } from '@/components/ui/dialog';
 import { DotsHorizontalIcon } from '@radix-ui/react-icons';
 import { Button } from '@/components/ui/button';
+import Rating from '../home-page/Rating';
+import Image from 'next/image';
+import { format } from 'date-fns';
+import { Comment } from './Profile';
+import { useMutation } from '@tanstack/react-query';
+import { useToast } from '../ui/use-toast';
+import { useSession } from 'next-auth/react';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
 
-export function CommentDialog({ text }: { text: string }) {
+export function CommentDialog({ comment, profilePic }: { comment: Comment; profilePic: string }) {
+  const { toast } = useToast();
+  const { data: sessionData } = useSession();
+  const router = useRouter();
+  const { mutate: deleteComment } = useMutation({
+    mutationFn: async () => {
+      return await axios.delete(`http://localhost:8081/comment/delete/${comment.id}`, {
+        headers: { Authorization: `Bearer ${sessionData?.user.authToken}` }
+      });
+    },
+    onSuccess: () => {
+      toast({
+        title: `Comentario do ponto de interesse ${comment.interestPoint.name} foi deletado!`,
+        variant: 'default',
+        className: 'bg-green-500 text-white'
+      });
+      router.refresh();
+    },
+    onError: () => {
+      toast({
+        title: `Não foi possivel deletar comentario do ponto de interesse ${comment.interestPoint.name}.`,
+        variant: 'destructive'
+      });
+    }
+  });
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -17,13 +46,39 @@ export function CommentDialog({ text }: { text: string }) {
         </button>
       </DialogTrigger>
       <DialogContent className='min-w-fit'>
-        <p className='whitespace-pre-wrap'>{text}</p>
-        <DialogFooter>
-          <DialogClose asChild>
-            <Button type='submit' variant='destructive'>
-              Fechar
-            </Button>
-          </DialogClose>
+        <div className='w-[500px] h-fit p-4'>
+          <div className='flex gap-3'>
+            <Image
+              height={90}
+              width={90}
+              alt={comment.tourist.touristName}
+              src={profilePic}
+              className='w-[90px] h-[90px] rounded-full object-cover'
+            />
+            <div>
+              <div className='flex gap-3 items-center'>
+                <h1 className='text-xl font-bold truncate'>{comment.tourist.touristName}</h1>
+                <p className='text-sm font-light'>
+                  Em {format(comment.wasVisitingDate, 'dd/MM/yyyy')}
+                </p>
+              </div>
+              <div>
+                <p className='text-sm font-light'>Avaliação</p>
+                <Rating rating={comment.rating} />
+              </div>
+            </div>
+          </div>
+          <p>{comment.text}</p>
+        </div>
+        <DialogFooter className='justify-between w-full'>
+          <Button
+            type='submit'
+            variant='destructive'
+            className='font-bold'
+            onClick={() => deleteComment()}
+          >
+            DELETAR
+          </Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
